5d193c1553bf25f948814969ba77f8115a96b5a1
mdraf <mohamamdrafi44@gmail.com>
2021-05-11 18:27:43 +0300

    commit 3

--- src/test/java/com/example/tests/DemoSiteTest.java
+++ src/test/java/com/example/tests/DemoSiteTest.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 
 public class DemoSiteTest extends UiBaseTest {
 
-  @Test(description = "Launch application Rafi Shaik")
+  @Test(description = "Launch application Rafi Shaik md")
   @Parameters(value = "tcId")
   public void launchApp(String tcId) {
     DemoSiteModule demoSiteModule = new DemoSiteModule();
